b4cf24947ab52a60f496492887cba05541537fb6
Nikita Kharashun <tsurai7walkin@gmail.com>
2023-12-17 11:44:31 +0300

    Add project files.

---
+++ AniMate-app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AniMate_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AniMate-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AniMate_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
