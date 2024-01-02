0727fbb056f027ec1239d923f0c0ad27c4aacd05
NikitaKalabin <115951587+NikitaKalabin@users.noreply.github.com>
2024-01-02 22:01:43 +0300

    Add media element

--- AniMate-app/MainPage.xaml.cs
+++ AniMate-app/MainPage.xaml.cs
@@ -7,6 +7,10 @@
         public MainPage()
         {
             InitializeComponent();
+
+            mediaElement.MediaOpened += MediaElement_MediaOpened;
+            mediaElement.MediaFailed += MediaElement_MediaFailed;
+            mediaElement.MediaEnded += MediaElement_MediaEnded;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +23,24 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void MediaElement_MediaOpened(object sender, EventArgs e)
+        {
+            // Handle media opened event
+            // This is triggered when the media is successfully opened
+        }
+
+        private void MediaElement_MediaFailed(object sender, EventArgs e)
+        {
+            // Handle media failed event
+            // This is triggered when there is an issue opening the media
+        }
+
+        private void MediaElement_MediaEnded(object sender, EventArgs e)
+        {
+            // Handle media ended event
+            // This is triggered when the media playback reaches the end
         }
     }
 }
--- AniMate-app/MauiProgram.cs
+++ AniMate-app/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
+using CommunityToolkit.Maui;
+
 namespace AniMate_app
 {
     public static class MauiProgram
@@ -7,18 +9,14 @@ namespace AniMate_app
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                })
-                .UseMauiCommunityToolkit();
+            builder.UseMauiApp<App>().UseMauiCommunityToolkit().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkitMediaElement();
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
