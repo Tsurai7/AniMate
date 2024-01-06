46f7ae9492378a1ded79ae2a08a933c1e0050341
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-01-06 13:50:40 +0300

    Add backend

---
+++ AniMate-backend/Data/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AniMate_backend.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string? Bio { get; set; }
+        public string? Image { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        //public List<string> LikedTitles { get; set; }
+        //public List<string> WatchHistory { get; set; }
+        //public List<string> Achievements { get; set; }
+    }
+}
---
+++ AniMate-backend/Models/Request/TitleRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace AniMate_backend.Models.TitleInfo
+{
+    public record TitleRequestDto
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("code")]
+        public string? Code { get; set; }
+
+        [JsonProperty("names")]
+        public NamesDto Names { get; set; }
+
+        [JsonProperty("genres")]
+        public List<string> Genres { get; set; }
+
+        [JsonProperty("player")]
+        public PlayerDto Player { get; set; }
+    }
+}
