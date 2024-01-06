6662ec93803d5d6fa4a7aea82e3b75a0912d837f
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-01-06 21:47:37 +0300

    Add anime service

---
+++ AniMate-app/Dto/EpisodeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AniMate_app.Dto
+{
+    public record EpisodeDto
+    {
+        public int Ordinal { get; set; }
+        public string Title { get; set; }
+        public string Fhd { get; set; }
+        public string Hd { get; set; }
+        public string Sd { get; set; }
+    }
+}
