03462a6ff2ee609c03f845ee8de0224111b79a42
nik.205520 <5203330@gmail.com>
2024-02-08 21:57:50 +0300

    fix buttons display

--- AniMate-app/Views/TitlePage.xaml.cs
+++ AniMate-app/Views/TitlePage.xaml.cs
@@ -1,4 +1,4 @@
-using AniMate_app.Services.AnilibriaService.Models;
+﻿using AniMate_app.Services.AnilibriaService.Models;
 using AniMate_app.ViewModels;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Layouts;
@@ -14,45 +14,19 @@ public partial class TitlePage : ContentPage
         InitializeComponent();
 
         BindingContext = viewModel = new TitleViewModel(title);
-
-        GenerateButtons();
     }
 
-    private void OnWatchButtonClicked(object sender, EventArgs e)
+    private async void OnWatchButtonClicked(object sender, EventArgs e)
     {
-        //mediaControl.IsVisible = true;
-    }
-
-    private void GenerateButtons()
-    {
-        StackLayout rowStackLayout = null;
-
-        for (int i = 1; i < viewModel.Title.Player.Episodes.Count; i++)
+        if (sender is Button button && button.CommandParameter is string hlsUrl)
         {
-            string episodeIndex = i.ToString();
-
-            Button button = new()
-            {
-                Text = $"����� {i}",
-                BackgroundColor = Color.FromRgb(169, 169, 169),
-                Margin = new Thickness(5, 5, 5, 5)
-            };
-            button.Clicked += async (sender, e) =>
-            {
-                await Navigation.PushAsync(new PlayerPage(viewModel.Title.Player.Episodes[episodeIndex].HlsUrls.Sd));
-            };
-
-            if (i % 4 == 1)
-            {
-                rowStackLayout = new StackLayout
-                {
-                    Orientation = StackOrientation.Horizontal,
-                };
-
-                ButtonContainer.Children.Add(rowStackLayout);         
-            }
-
-            rowStackLayout.Children.Add(button);
+            await Navigation.PushAsync(new PlayerPage(hlsUrl));
+            // Переход на страницу PlayerPage с использованием hlsUrl
+            // Например:
+            // await Navigation.PushAsync(new PlayerPage(hlsUrl));
         }
+        
     }
+
+    
 }
