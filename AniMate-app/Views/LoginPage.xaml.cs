417c087de5864d185ee9fd6bb78a0fa2d9c765ac
nik.205520 <5203330@gmail.com>
2024-02-12 11:23:57 +0300

    Add register and login pages

---
+++ AniMate-app/Views/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace AniMate_app.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        // Ваш код для аутентификации пользователя
+        // Проверка логина и пароля и т.д.
+        // Если вход успешный, выполните нужные действия, например, переход на следующую страницу
+        await Navigation.PushAsync(new ProfilePage()); // Переход на основную страницу приложения
+    }
+
+    private async void RegisterButton_Clicked(object sender, EventArgs e)
+    {
+        // Ваш код для обработки нажатия на кнопку регистрации
+        // Например, переход на страницу регистрации
+        await Navigation.PushAsync(new RegistrationPage());
+    }
+}
---
+++ AniMate-app/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AniMate_app.Views;
+
+public partial class RegistrationPage : ContentPage
+{
+	public RegistrationPage()
+	{
+		InitializeComponent();
+	}
+}
