f6e17ab7ea61e4c26b597466d95ee9d099386ff4
nik.205520 <5203330@gmail.com>
2024-02-20 05:26:12 +0300

    switch to media element, try to make button for exit

--- AniMate-app/Views/PlayerPage.xaml.cs
+++ AniMate-app/Views/PlayerPage.xaml.cs
@@ -1,4 +1,6 @@
 using AniMate_app.ViewModels;
+using CommunityToolkit.Maui.Core.Primitives;
+using CommunityToolkit.Maui.Views;
 
 namespace AniMate_app.Views;
 
@@ -9,9 +11,12 @@ public partial class PlayerPage : ContentPage
 	public PlayerPage()
 	{
 		InitializeComponent();
-
+        AppShell.SetNavBarIsVisible(this, false);
+        AppShell.SetTabBarIsVisible(this, false);
         BindingContext = viewModel = new PlayerViewModel();
-	}
+
+
+    }
 
     protected override void OnDisappearing()
     {
@@ -22,5 +27,36 @@ public partial class PlayerPage : ContentPage
     private void PauseVideo()
     {
         mediaControl.Pause();
+        mediaControl.Handler?.DisconnectHandler();
+    }
+
+    private async void OnMediaElementTapped(object sender, TappedEventArgs e)
+    {
+        if(BackButton.IsVisible != true)
+        {
+            BackButton.IsVisible = true;
+        }
+        else
+        {
+            BackButton.IsVisible = false;
+        }
+
+        await Task.Delay(5000);
+        BackButton.IsVisible = false;
+    }
+
+    private async void OnMediaElementStateChanged(object sender, MediaStateChangedEventArgs e)
+    {
+        if (BackButton.IsVisible != true)
+        {
+            BackButton.IsVisible = true;
+        }
+        else
+        {
+            BackButton.IsVisible = false;
+        }
+
+        await Task.Delay(5000);
+        BackButton.IsVisible = false;
     }
 }
