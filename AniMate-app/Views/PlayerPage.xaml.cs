d5b15eef22dbe32ef8de6c5179ed10dd9f7e42a3
nik.205520 <5203330@gmail.com>
2024-02-20 11:16:31 +0300

    delete button logic, enable nav bar on player page

--- AniMate-app/Views/PlayerPage.xaml.cs
+++ AniMate-app/Views/PlayerPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class PlayerPage : ContentPage
 	public PlayerPage()
 	{
 		InitializeComponent();
-        AppShell.SetNavBarIsVisible(this, false);
+        AppShell.SetNavBarIsVisible(this, true);
         AppShell.SetTabBarIsVisible(this, false);
         BindingContext = viewModel = new PlayerViewModel();
 
@@ -28,35 +28,5 @@ public partial class PlayerPage : ContentPage
     {
         mediaControl.Pause();
         mediaControl.Handler?.DisconnectHandler();
-    }
-
-    private async void OnMediaElementTapped(object sender, TappedEventArgs e)
-    {
-        if(BackButton.IsVisible != true)
-        {
-            BackButton.IsVisible = true;
-        }
-        else
-        {
-            BackButton.IsVisible = false;
-        }
-
-        await Task.Delay(5000);
-        BackButton.IsVisible = false;
-    }
-
-    private async void OnMediaElementStateChanged(object sender, MediaStateChangedEventArgs e)
-    {
-        if (BackButton.IsVisible != true)
-        {
-            BackButton.IsVisible = true;
-        }
-        else
-        {
-            BackButton.IsVisible = false;
-        }
-
-        await Task.Delay(5000);
-        BackButton.IsVisible = false;
     }
 }
