246fb7257763a6df088feaedc65bcfa3658d1bb4
Егор Гоков <caseharo.e@gmail.com>
2024-03-01 19:58:34 +0300

    small code refactoring

--- AniMate-app/Views/TitlePage.xaml.cs
+++ AniMate-app/Views/TitlePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using AniMate_app.Services.AnilibriaService.Models;
-using AniMate_app.ViewModels;
+﻿using AniMate_app.ViewModels;
 
 namespace AniMate_app.Views;
 
@@ -19,45 +18,29 @@ public partial class TitlePage : ContentPage
     private async void OnWatchButtonClicked(object sender, EventArgs e)
     {
         if (sender is Button button && button.CommandParameter is string hlsUrl)
-        {
             await Shell.Current.GoToAsync($"playerpage?mediaurl={hlsUrl}" );
-            //await Navigation.PushAsync(new PlayerPage(hlsUrl));
-        }
-        
     }
 
     private void OnTextRecognizerTap(object sender, TappedEventArgs e)
     {
-        FormattedString formattedString = new FormattedString();
+        FormattedString formattedString = new();
+
+        formattedString.Spans.Add(new Span
+        {
+            Text = isFullDescriptionOpen ? viewModel.ShortDescription : viewModel.Title.RuDescription,
+        });
 
         if (isFullDescriptionOpen)
         {
             formattedString.Spans.Add(new Span
             {
-                Text = viewModel.ShortDescription,
-                
-            });
-            formattedString.Spans.Add(new Span
-            {
                 Text = "... ещё",
                 TextColor = Colors.Grey,
-                
             });
-            descriptionLabel.FormattedText = formattedString;
-            isFullDescriptionOpen= false;
         }
-        else
-        {
-            formattedString.Spans.Add(new Span
-            {
-                Text = viewModel.Title.RuDescription,
-                FontSize = 15,
 
-            }); ;
-            descriptionLabel.FormattedText = formattedString;
-            isFullDescriptionOpen= true;
-        }
-        
+        descriptionLabel.FormattedText = formattedString;
 
+        isFullDescriptionOpen = !isFullDescriptionOpen;
     }
 }
