cfbf55a0e3fce40b81b3eeb0016cd8db288dab0b
Егор Гоков <caseharo.e@gmail.com>
2024-03-05 13:30:31 +0300

    test splash screen update

--- AniMate-app/Platforms/Android/MainActivity.cs
+++ AniMate-app/Platforms/Android/MainActivity.cs
@@ -2,10 +2,11 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace AniMate_app
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", LaunchMode = LaunchMode.SingleTask, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/AppTheme.Starting", LaunchMode = LaunchMode.SingleTask, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     [IntentFilter(
         [Intent.ActionView],
         Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable],
@@ -26,9 +27,13 @@ namespace AniMate_app
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            AndroidX.Core.SplashScreen.SplashScreen.InstallSplashScreen(this);
+
             base.OnCreate(savedInstanceState);
 
             ProcessIntent(Intent);
+
+            System.Threading.Thread.Sleep(1000);
         }
 
         private void ProcessIntent(Intent intent)
