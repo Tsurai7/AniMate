650f52292b00fd4e81817fd929c1d3cf01aa9963
Егор Гоков <caseharo.e@gmail.com>
2024-04-28 15:23:27 +0300

    link association fix(last visited issue, cut out)

--- AniMate-app/ViewModels/TitleViewModel.cs
+++ AniMate-app/ViewModels/TitleViewModel.cs
@@ -9,15 +9,9 @@ namespace AniMate_app.ViewModels
     {
         private readonly AnilibriaService _service;
 
-        private readonly Stack<string> _lastVisited;
-
         public TitleViewModel(AnilibriaService anilibriaService)
         {
             _service = anilibriaService;
-
-            var lastVisited = Preferences.Default.Get<string>("visited", default);
-
-            _lastVisited = lastVisited is null ? new() : new(lastVisited.Split(';'));
         }
 
         private Title _title;
@@ -51,16 +45,5 @@ namespace AniMate_app.ViewModels
 
         [ObservableProperty]
         private string _shortDescription;
-
-        public void OnNavigatedTo()
-        {
-            while(_lastVisited.Count > 4)
-                _lastVisited.Pop();
-
-            if(!_lastVisited.Contains(Title.Code))
-                _lastVisited.Push(Title.Code);
-
-            Preferences.Default.Set<string>("visited", string.Join(';', _lastVisited));
-        }
     }
 }
--- AniMate-app/Views/TitlePage.xaml.cs
+++ AniMate-app/Views/TitlePage.xaml.cs
@@ -15,13 +15,6 @@ public partial class TitlePage : ContentPage
         BindingContext = _viewModel = titleViewModel;
     }
 
-    protected override void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        base.OnNavigatedTo(args);
-
-        _viewModel.OnNavigatedTo();
-    }
-
     private async void OnWatchButtonClicked(object sender, EventArgs e)
     {
         if (sender is Button button && button.CommandParameter is string hlsUrl)
