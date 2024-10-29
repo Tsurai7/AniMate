d7cdb77d01430ade51aabf3be22be878e3857555
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-10-29 17:32:18 +0300

    Add startup

--- AniMate.Backend/Backend.API/Program.cs
+++ AniMate.Backend/Backend.API/Program.cs
@@ -1,47 +1,10 @@
+using Backend.API;
 using Backend.API.Hubs;
-using Backend.Application;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:5002");
-builder.Services.AddApplication();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSignalR();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-builder.Services.AddControllers().AddNewtonsoftJson();;
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Animate", Version = "v1" });
-    
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter your token in the text input below."
-    });
-    
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
+builder.Services.ConfigureServices();
 
 var app = builder.Build();
 
