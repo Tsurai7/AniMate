415fbcd3c2e6d8b29fdef67e604ea6e4471f60a2
Nikita Kharashun <nikita.kharashun@idt.net>
2025-02-15 20:49:36 +0300

    remove explicit address definition

--- AniMate.Backend/Backend.API/Program.cs
+++ AniMate.Backend/Backend.API/Program.cs
@@ -3,7 +3,6 @@ using Backend.API.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5002");
 builder.Services.ConfigureServices();
 
 var app = builder.Build();
