f6166e56a72fa434831e6c0ecb1d50be560466c2
Nikita Kharashun <tsurai7walkin@gmail.com>
2025-03-04 09:52:24 +0300

    posters fix

--- AniMate.Backend/Backend.API/Controllers/Models/Titles/Title.cs
+++ AniMate.Backend/Backend.API/Controllers/Models/Titles/Title.cs
@@ -1,18 +1,53 @@
+using System.Text.Json.Serialization;
 using Backend.Domain.Models.Anime;
 
 namespace Backend.API.Controllers.Models.Titles;
 
 public class Title
 {
-    public int Id { get; set; }
-    public string Code { get; set; }
-    public NamesDto Names { get; set; }
-    public List<FranchiseDto>? Franchises { get; set; }
-    public StatusDto Status { get; set; }
-    public PostersDto Posters { get; set; }
-    public TypeDto? Type { get; set; }
-    public List<string> Genres { get; set; }
-    public SeasonDto Season { get; set; }
-    public string RuDescription { get; set; }
-    public PlayerDto Player { get; set; }
+    public int Id { get; init; }
+    public string Code { get; init; }
+    public NamesDto Names { get; init; }
+    public List<FranchiseDto>? Franchises { get; init; }
+    public StatusDto Status { get; init; }
+    public Posters Posters { get; init; }
+    public TypeDto? Type { get; init; }
+    public List<string> Genres { get; init; }
+    public SeasonDto Season { get; init; }
+    public string RuDescription { get; init; }
+    public Player Player { get; init; }
+}
+
+public class Posters
+{
+    public PosterSize Small { get; init; }
+    public PosterSize Medium { get; init; }
+    public PosterSize Original { get; init; }
+}
+
+public class PosterSize
+{
+    public string Url { get; set; }
+}
+
+public class Player
+{
+    public string? AlternativePlayer { get; init; }
+    public string Host { get; init; }
+    [JsonPropertyName("list")]
+    public Dictionary<string, EpisodeDto> Episodes { get; init; }
+}
+
+public class EpisodeDto
+{
+    public int Episode { get; init; }
+    public string? Name { get; init; }
+    public Hls HlsUrls { get; init; }
+}
+
+public class Hls
+{
+    public string Fhd { get; init; }
+    public string Hd { get; init; }
+    public string Sd { get; init; }
 }
